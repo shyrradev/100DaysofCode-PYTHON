dad74ff68bfb289d2bd76024adc20c0974ffb936
shyrradev <shayanobrohi@gmail.com>
2024-06-20 01:07:51 +0500

    Added the tip calculator program and updated README.md file

---
+++ p02-tip-calculator/README.md
@@ -0,0 +1,10 @@
+# Tip Calculator
+
+This is a simple Python script that calculates the tip amount and the total amount each person should pay when splitting a bill.
+
+## Usage
+
+The script prompts the user to enter the total bill amount, the desired tip percentage, and the number of people splitting the bill. It then calculates the tip amount and the total amount each person should pay, rounding to two decimal places for accuracy.
+
+### Example
+
--- p02-tip-calculator/tip-calculator.py
+++ p02-tip-calculator/tip-calculator.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python3
+
+
+print("Welcome to the Tip Calculator")
+
+total_bill=float((input("what was the total bill? ")))
+tip=float((input("how much would you like to tip? 10, 12, or 15% ")))
+bill_split=float(input("how many people are splitting the bill? "))
+
+tip_amount=(total_bill * tip / 100)
+
+total_bill_tip= total_bill + tip_amount
+split_amount=total_bill_tip / bill_split
+
+final_tip_amount=round(tip_amount, 2)
+final_split_amount=round(split_amount, 2)
+
+print(f"The tip is {final_tip_amount} and the bill split amount is {final_split_amount}")
 
